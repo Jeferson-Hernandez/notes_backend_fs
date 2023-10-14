fb4665e5b5a806bdd0849392c1023f8f366fd712
Jeferson-Hernandez <jefry_2558@hotmail.com>
2023-10-14 13:05:09 -0500

    test

--- index.js
+++ index.js
@@ -44,10 +44,10 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
+app.get('/api/notes', (request, response, next) => {
     Note.find({}).then(notes => {
         response.json(notes)
-    })
+    }).catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response, next) => {
